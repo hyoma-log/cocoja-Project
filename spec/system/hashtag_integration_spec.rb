bc5ecd44471c1b493e7ee3777e43bedc0c6a281f
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-12-28 03:07:15 +0900

    fix: ハッシュタグ機能のSystem Spec修正および全テストスイートの完遂

--- spec/system/hashtag_integration_spec.rb
+++ spec/system/hashtag_integration_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'ハッシュタグ機能', type: :system do
   let!(:prefecture) { create(:prefecture, name: '東京都') }
 
   before do
+    user.confirm if user.respond_to?(:confirm)
     sign_in user
     driven_by(:rack_test)
   end
@@ -13,9 +14,12 @@ RSpec.describe 'ハッシュタグ機能', type: :system do
     context 'when 新規投稿を作成する場合' do
       it '投稿が保存され、ハッシュタグが作成されること' do
         visit new_post_path
-        select prefecture.name, from: 'post[prefecture_id]'
+        select prefecture.name, from: '都道府県' rescue select prefecture.name, from: 'post[prefecture_id]'
         fill_in 'post[content]', with: 'テスト投稿です #観光 #グルメ'
         click_button '投稿する'
+
+        # 投稿が保存されているか確認（リダイレクト後）
+        expect(page).to have_content 'テスト投稿です'
 
         post = Post.last
         expect(post.hashtags.pluck(:name)).to contain_exactly('観光', 'グルメ')
@@ -25,6 +29,7 @@ RSpec.describe 'ハッシュタグ機能', type: :system do
         post = create(:post, user: user, prefecture: prefecture, content: 'テスト投稿です #観光 #グルメ')
         visit post_path(post)
 
+        expect(page).not_to have_content 'メールアドレスの確認が必要です'
         expect(page).to have_content 'テスト投稿です'
         expect(page).to have_content '#観光'
         expect(page).to have_content '#グルメ'
@@ -45,8 +50,8 @@ RSpec.describe 'ハッシュタグ機能', type: :system do
       end
 
       it 'ハッシュタグページで関連投稿のみが表示されること' do
-        tag = Hashtag.find_by(name: '観光')
-        visit hashtag_posts_path(name: tag.name)
+        tag_name = '観光'
+        visit "/posts/hashtag/#{CGI.escape(tag_name)}"
 
         expect(page).to have_content '観光スポット'
         expect(page).not_to have_content 'ランチ'
@@ -56,8 +61,8 @@ RSpec.describe 'ハッシュタグ機能', type: :system do
         visit posts_path
         expect(page).to have_content '観光スポット'
 
-        tag = Hashtag.find_by(name: '観光')
-        visit hashtag_posts_path(name: tag.name)
+        tag_name = '観光'
+        visit "/posts/hashtag/#{CGI.escape(tag_name)}"
 
         expect(page).to have_content '観光スポット'
         expect(page).to have_content 'ハッシュタグ「#観光」の投稿'
