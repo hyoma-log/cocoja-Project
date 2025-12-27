523803d0aab2487612367f05baa8224dffdcf82f
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-12-28 05:18:56 +0900

    chore: Rails 7.2へのアップデートとRuboCop違反の修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,16 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    if Rails.env.development?
-      origins 'localhost:3000'
-    else
-      origins ENV.fetch('ALLOWED_ORIGINS') {
-        'https://www.cocoja.jp/'
-      }
-    end
+# Be sure to restart your server when you modify this file.
 
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true
-  end
-end
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+
+# Read more: https://github.com/cyu/rack-cors
+
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins "example.com"
+#
+#     resource "*",
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
