eb3b537d20834fbbf543b0f2abd7b76ac867261e
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-12-28 11:39:18 +0900

    fix: Rails 7.2への完全移行とデプロイ時のエラー修正

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Myapp
   class Application < Rails::Application
-    config.load_defaults 7.1 # ここを 7.2 に上げてもOKですが、まずはロケールを直しましょう
+    config.load_defaults 7.2 # ここを 7.2 に上げてもOKですが、まずはロケールを直しましょう
 
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # --- ここから追記 ---
     # 日本の時間帯に設定
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
@@ -21,7 +20,6 @@ module Myapp
     config.i18n.default_locale = :ja
 
     # 複数のロケールファイル（config/locales/**/*.yml）を読み込む設定
-    config.i18n.load_path += Rails.root.glob('config/locales/**/*.{rb,yml}')
-    # --- ここまで追記 ---
+    config.i18n.load_path += Rails.root.glob('config/locales/**/*.{rb,yml}').map(&:to_s)
   end
 end
