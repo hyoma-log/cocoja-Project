ab5b4c4c6bb7845b4d4bac15c1132c844c6a0ba3
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2026-01-02 20:01:59 +0900

    fix: デバックテスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :log_request_details if Rails.env.production?
 
   def after_sign_in_path_for(_resource)
     top_page_login_url(protocol: 'https')
   end
 
   private
+
+  def log_request_details
+    Rails.logger.info "DEBUG: IP=#{request.ip}, RemoteIP=#{request.remote_ip}, X-Forwarded-Proto=#{request.headers['X-Forwarded-Proto']}, SSL?=#{request.ssl?}"
+  end
 
   def redirect_if_authenticated
     return unless user_signed_in?
