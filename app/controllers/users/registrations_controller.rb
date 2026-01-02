b2c3180350c488475f49bcf36c0159a3db44877c
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2026-01-02 21:49:09 +0900

    fix: デバック

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,9 +2,24 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :configure_sign_up_params, only: [:create]
 
-    # def create
-    #   super
-    # end
+    def create
+      Rails.logger.info 'DEBUG: Starting RegistrationsController#create'
+      begin
+        super do |resource|
+          Rails.logger.info "DEBUG: User saved? #{resource.persisted?}"
+          if resource.persisted?
+            Rails.logger.info "DEBUG: User ID: #{resource.id}"
+          else
+            Rails.logger.error "DEBUG: User save FAILED. Errors: #{resource.errors.full_messages}"
+          end
+        end
+      rescue StandardError => e
+        Rails.logger.error "DEBUG: FATAL EXCEPTION in RegistrationsController#create: #{e.class} - #{e.message}"
+        Rails.logger.error e.backtrace.join("\n")
+        raise e
+      end
+      Rails.logger.info 'DEBUG: Finished RegistrationsController#create'
+    end
 
     protected
 
