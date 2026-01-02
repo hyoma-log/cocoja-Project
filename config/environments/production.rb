92c38a072bcc991dd4da083a4cefe3722d499125
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2026-01-02 20:50:35 +0900

    fix: エラーを無視して進むに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
     read_timeout:         5
   }
   config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 end
