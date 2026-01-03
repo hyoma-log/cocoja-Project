925fd7ec2a5df7444d1cb83a7498cf0d239de28d
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2026-01-04 01:05:06 +0900

    fix: デバック7

--- app/middleware/force_scheme_middleware.rb
+++ app/middleware/force_scheme_middleware.rb
@@ -4,9 +4,16 @@ class ForceSchemeMiddleware
   end
 
   def call(env)
-    if env['HTTP_X_FORWARDED_PROTO'] == 'https'
+    proto = env['HTTP_X_FORWARDED_PROTO']
+    Rails.logger.info "DEBUG: Middleware raw proto: '#{proto}'"
+    Rails.logger.info "DEBUG: Middleware keys: #{env.keys.select { |k| k.include?('FORWARDED') }}"
+
+    if proto == 'https'
       env['rack.url_scheme'] = 'https'
       env['HTTPS'] = 'on'
+      Rails.logger.info 'DEBUG: Middleware forced HTTPS'
+    else
+      Rails.logger.info 'DEBUG: Middleware DID NOT force HTTPS'
     end
     @app.call(env)
   end
