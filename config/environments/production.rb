770a6c9f692dbfd43845a7d586658a77724e7a36
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2026-01-04 14:35:17 +0900

    fix: ドメインアクセスを更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,13 +46,13 @@ Rails.application.configure do
   config.action_cable.disable_request_forgery_protection = false
   config.action_cable.allowed_request_origins = [%r{http://*}, %r{https://*}]
 
-  config.action_mailer.default_url_options = { host: ENV.fetch('APP_DOMAIN', 'www.cocoja.jp'),
+  config.action_mailer.default_url_options = { host: ENV.fetch('APP_DOMAIN', 'cocoja-613ycaap.b4a.run'),
 protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               ENV.fetch('APP_DOMAIN', 'www.cocoja.jp'),
+    domain:               ENV.fetch('APP_DOMAIN', 'cocoja-613ycaap.b4a.run'),
     user_name:            Rails.application.credentials.dig(:gmail, :username),
     password:             Rails.application.credentials.dig(:gmail, :password),
     authentication:       'plain',
